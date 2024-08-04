21d4bb72f28c348cb4e87f968245d1d63351e877
tarequr <tarequrr8@gmail.com>
2024-08-04 23:37:10 +0600

    Implement Laravel queue functionality

--- app/Notifications/RegisteredUserNotification.php
+++ app/Notifications/RegisteredUserNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class RegisteredUserNotification extends Notification
+class RegisteredUserNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
